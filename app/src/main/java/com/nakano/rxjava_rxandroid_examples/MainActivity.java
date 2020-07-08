0faa5ff9b49482bfa6bec240428270bfa72a9eb7
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-08 23:21:27 +0900

    add Create Operators - fromArray, fromIterable, fromCallable
    https://codingwithmitch.com/courses/rxjava-rxandroid-for-beginners/rxjava-operators-from-array-iterable-callable/

--- app/src/main/java/com/nakano/rxjava_rxandroid_examples/MainActivity.java
+++ app/src/main/java/com/nakano/rxjava_rxandroid_examples/MainActivity.java
@@ -5,7 +5,10 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.concurrent.TimeUnit;
+import com.nakano.rxjava_rxandroid_examples.models.Task;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
@@ -25,232 +28,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Observable<Task> taskObservable = Observable // create a new Observable object
-//                .fromIterable(DataSource.createTasksList()) // apply 'fromIterable' operator
-//                .subscribeOn(Schedulers.io()) // designate worker thread (background)
-//                .filter(new Predicate<Task>() {
-//                    @Override
-//                    public boolean test(Task task) throws Exception {
-//                        Log.d(TAG, "test: " + Thread.currentThread().getName());
-//                        try {
-//                            Thread.sleep(1000);
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
-//                        return task.isComplete();
-//                    }
-//                })
-//                .observeOn(AndroidSchedulers.mainThread()); // designate observer thread (main thread)
+//        Task[] list = new Task[5];
+//        list[0] = (new Task("Take out the trash", true, 3));
+//        list[1] = (new Task("Walk the dog", false, 2));
+//        list[2] = (new Task("Make my bed", true, 1));
+//        list[3] = (new Task("Unload the dishwasher", false, 0));
+//        list[4] = (new Task("Make dinner", true, 5));
+//
+//        Observable<Task> taskObservable = Observable
+//                .fromArray(list)
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread());
 //
 //        taskObservable.subscribe(new Observer<Task>() {
-//            @Override
-//            public void onSubscribe(Disposable d) {
-//                Log.d(TAG, "onSubscribe: called.");
-//                disposable.add(d);
-//            }
-//
-//            @Override
-//            public void onNext(Task task) { // run on main thread
-//                Log.d(TAG, "onNext: : " + Thread.currentThread().getName());
-//                Log.d(TAG, "onNext: : " + task.getDescription());
-//            }
-//
-//            @Override
-//            public void onError(Throwable e) {
-//                Log.e(TAG, "onError: called.", e);
-//            }
-//
-//            @Override
-//            public void onComplete() {
-//                Log.d(TAG, "onComplete: called.");
-//            }
-//        });
-
-//        // Instantiate the object to become an Observable
-//        final Task task = new Task("Walk the dog", false, 4);
-//
-//        // Create the Observable
-//        Observable<Task> singleTaskObservable = Observable
-//                .create(new ObservableOnSubscribe<Task>() {
-//                    @Override
-//                    public void subscribe(ObservableEmitter<Task> emitter) throws Exception {
-//                        if(!emitter.isDisposed()){
-//                            emitter.onNext(task);
-//                            emitter.onComplete();
-//                        }
-//                    }
-//                })
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread());
-//
-//        // Subscribe to the Observable and get the emitted object
-//        singleTaskObservable.subscribe(new Observer<Task>() {
-//            @Override
-//            public void onSubscribe(Disposable d) {
-//                Log.d(TAG, "onSubscribe: called.");
-//            }
-//
-//            @Override
-//            public void onNext(Task task) { // run on main thread
-//                Log.d(TAG, "onNext: : " + Thread.currentThread().getName());
-//                Log.d(TAG, "onNext: single task: " + task.getDescription());
-//            }
-//
-//            @Override
-//            public void onError(Throwable e) {
-//                Log.e(TAG, "onError: called.", e);
-//            }
-//
-//            @Override
-//            public void onComplete() {
-//                Log.d(TAG, "onComplete: called.");
-//            }
-//        });
-
-//        // Create the Observable
-//        Observable<Task> taskListObservable = Observable
-//                .create(new ObservableOnSubscribe<Task>() {
-//                    @Override
-//                    public void subscribe(ObservableEmitter<Task> emitter) throws Exception {
-//
-//                        // Inside the subscribe method iterate through the list of tasks and call onNext(task)
-//                        for(Task task: DataSource.createTasksList()){
-//                            if(!emitter.isDisposed()){
-//                                emitter.onNext(task);
-//                            }
-//                        }
-//                        // Once the loop is complete, call the onComplete() method
-//                        if(!emitter.isDisposed()){
-//                            emitter.onComplete();
-//                        }
-//
-//                    }
-//                })
-//                .subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread());
-//
-//        // Subscribe to the Observable and get the emitted objects
-//        taskListObservable.subscribe(new Observer<Task>() {
-//            @Override
-//            public void onSubscribe(Disposable d) {
-//                Log.d(TAG, "onSubscribe: called.");
-//            }
-//
-//            @Override
-//            public void onNext(Task task) { // run on main thread
-//                Log.d(TAG, "onNext: : " + Thread.currentThread().getName());
-//                Log.d(TAG, "onNext: task list: " + task.getDescription());
-//            }
-//
-//            @Override
-//            public void onError(Throwable e) {
-//                Log.e(TAG, "onError: called.", e);
-//            }
-//
-//            @Override
-//            public void onComplete() {
-//                Log.d(TAG, "onComplete: called.");
-//            }
-//        });
-
-//        Observable.just("first", "second", "third", "fourth", "fifth", "sixth",
-//                "seventh", "eighth", "ninth", "tenth")
-//                .subscribeOn(Schedulers.io()) // What thread to do the work on
-//                .observeOn(AndroidSchedulers.mainThread()) // What thread to observe the results on
-//                .subscribe(new Observer<String>() { // view the results by creating a new observer
-//                    @Override
-//                    public void onSubscribe(Disposable d) {
-//                        Log.d(TAG, "onSubscribe: called");
-//                    }
-//
-//                    @Override
-//                    public void onNext(String s) {
-//                        Log.d(TAG, "onNext: " + s);
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable e) {
-//                        Log.e(TAG, "onError: ", e);
-//                    }
-//
-//                    @Override
-//                    public void onComplete() {
-//                        Log.d(TAG, "onComplete: done...");
-//                    }
-//                });
-
-//        Observable.range(0,11)
-//                .observeOn(Schedulers.io())
-//                .subscribeOn(AndroidSchedulers.mainThread())
-//                .subscribe(new Observer<Integer>() {
-//                    @Override
-//                    public void onSubscribe(Disposable d) {
-//                        Log.d(TAG, "onSubscribe: called");
-//                    }
-//
-//                    @Override
-//                    public void onNext(Integer integer) {
-//                        Log.d(TAG, "onNext: " + integer);
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable e) {
-//                        Log.e(TAG, "onError: ", e);
-//                    }
-//
-//                    @Override
-//                    public void onComplete() {
-//                        Log.d(TAG, "onComplete: done...");
-//                    }
-//                });
-
-//        Observable.range(0,3)
-//                .repeat(2)
-//                .observeOn(Schedulers.io())
-//                .subscribeOn(AndroidSchedulers.mainThread())
-//                .subscribe(new Observer<Integer>() {
-//                    @Override
-//                    public void onSubscribe(Disposable d) {
-//                        Log.d(TAG, "onSubscribe: called");
-//                    }
-//
-//                    @Override
-//                    public void onNext(Integer integer) {
-//                        Log.d(TAG, "onNext: " + integer);
-//                    }
-//
-//                    @Override
-//                    public void onError(Throwable e) {
-//                        Log.e(TAG, "onError: ", e);
-//                    }
-//
-//                    @Override
-//                    public void onComplete() {
-//                        Log.d(TAG, "onComplete: done...");
-//                    }
-//                });
-
-//        // emit an observable every time interval
-//        Observable<Long> intervalObservable = Observable
-//                .interval(1, TimeUnit.SECONDS)
-//                .subscribeOn(Schedulers.io())
-//                .takeWhile(new Predicate<Long>() { // stop the process if more than 5 seconds passes
-//                    @Override
-//                    public boolean test(Long aLong) throws Exception {
-//                        return aLong <= 5;
-//                    }
-//                })
-//                .observeOn(AndroidSchedulers.mainThread());
-//
-//        intervalObservable.subscribe(new Observer<Long>() {
 //            @Override
 //            public void onSubscribe(Disposable d) {
 //                Log.d(TAG, "onSubscribe: called");
 //            }
 //
 //            @Override
-//            public void onNext(Long aLong) {
-//                Log.d(TAG, "onNext: interval: " + aLong);
+//            public void onNext(Task task) {
+//                Log.d(TAG, "onNext: : " + task.getDescription());
 //            }
 //
 //            @Override
@@ -264,32 +62,37 @@ public class MainActivity extends AppCompatActivity {
 //            }
 //        });
 
-        // emit single observable after a given delay
-        Observable<Long> timeObservable = Observable
-                .timer(3, TimeUnit.SECONDS)
+        List<Task> taskList = new ArrayList<>();
+        taskList.add(new Task("Take out the trash", true, 3));
+        taskList.add(new Task("Walk the dog", false, 2));
+        taskList.add(new Task("Make my bed", true, 1));
+        taskList.add(new Task("Unload the dishwasher", false, 0));
+        taskList.add(new Task("Make dinner", true, 5));
+
+        Observable<Task> taskObservable = Observable
+                .fromIterable(taskList)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
 
-        timeObservable.subscribe(new Observer<Long>() {
-
-            long time = 0; // variable for demonstating how much time has passed
-
+        taskObservable.subscribe(new Observer<Task>() {
             @Override
             public void onSubscribe(Disposable d) {
-                time = System.currentTimeMillis() / 1000;
-                Log.d(TAG, "onSubscribe: called");
+
             }
+
             @Override
-            public void onNext(Long aLong) {
-                Log.d(TAG, "onNext: " + ((System.currentTimeMillis() / 1000) - time) + " seconds have elapsed." );
+            public void onNext(Task task) {
+                Log.d(TAG, "onNext: : " + task.getDescription());
             }
+
             @Override
             public void onError(Throwable e) {
-                Log.e(TAG, "onError: ", e);
+
             }
+
             @Override
             public void onComplete() {
-                Log.d(TAG, "onComplete: done...");
+
             }
         });
     }
