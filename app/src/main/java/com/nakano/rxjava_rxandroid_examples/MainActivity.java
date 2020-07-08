07287d43ad36832d157c1af7cdf12846a33f7204
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-08 22:55:39 +0900

    add Create Operators - Interval and Timer
    https://codingwithmitch.com/courses/rxjava-rxandroid-for-beginners/rx-operators-interval-timer/

--- app/src/main/java/com/nakano/rxjava_rxandroid_examples/MainActivity.java
+++ app/src/main/java/com/nakano/rxjava_rxandroid_examples/MainActivity.java
@@ -5,6 +5,14 @@ import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -221,6 +229,69 @@ public class MainActivity extends AppCompatActivity {
 //                        Log.d(TAG, "onComplete: done...");
 //                    }
 //                });
+
+//        // emit an observable every time interval
+//        Observable<Long> intervalObservable = Observable
+//                .interval(1, TimeUnit.SECONDS)
+//                .subscribeOn(Schedulers.io())
+//                .takeWhile(new Predicate<Long>() { // stop the process if more than 5 seconds passes
+//                    @Override
+//                    public boolean test(Long aLong) throws Exception {
+//                        return aLong <= 5;
+//                    }
+//                })
+//                .observeOn(AndroidSchedulers.mainThread());
+//
+//        intervalObservable.subscribe(new Observer<Long>() {
+//            @Override
+//            public void onSubscribe(Disposable d) {
+//                Log.d(TAG, "onSubscribe: called");
+//            }
+//
+//            @Override
+//            public void onNext(Long aLong) {
+//                Log.d(TAG, "onNext: interval: " + aLong);
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                Log.e(TAG, "onError: ", e);
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//                Log.d(TAG, "onComplete: done...");
+//            }
+//        });
+
+        // emit single observable after a given delay
+        Observable<Long> timeObservable = Observable
+                .timer(3, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        timeObservable.subscribe(new Observer<Long>() {
+
+            long time = 0; // variable for demonstating how much time has passed
+
+            @Override
+            public void onSubscribe(Disposable d) {
+                time = System.currentTimeMillis() / 1000;
+                Log.d(TAG, "onSubscribe: called");
+            }
+            @Override
+            public void onNext(Long aLong) {
+                Log.d(TAG, "onNext: " + ((System.currentTimeMillis() / 1000) - time) + " seconds have elapsed." );
+            }
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: ", e);
+            }
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: done...");
+            }
+        });
     }
 
     @Override
