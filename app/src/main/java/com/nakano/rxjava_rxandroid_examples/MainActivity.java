c6b46042f74930230d00b2dff62c4fb8dba04813
kazudmb <nakano.kazuki.dmb@gmail.com>
2020-07-11 23:07:27 +0900

    add Filter Operators - Filter
    https://codingwithmitch.com/courses/rxjava-rxandroid-for-beginners/rx-operators-filter/

--- app/src/main/java/com/nakano/rxjava_rxandroid_examples/MainActivity.java
+++ app/src/main/java/com/nakano/rxjava_rxandroid_examples/MainActivity.java
@@ -6,14 +6,13 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.nakano.rxjava_rxandroid_examples.models.Task;
-
-import java.util.ArrayList;
-import java.util.List;
+import com.nakano.rxjava_rxandroid_examples.util.DataSource;
 
 import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 
 
@@ -28,49 +27,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Task[] list = new Task[5];
-//        list[0] = (new Task("Take out the trash", true, 3));
-//        list[1] = (new Task("Walk the dog", false, 2));
-//        list[2] = (new Task("Make my bed", true, 1));
-//        list[3] = (new Task("Unload the dishwasher", false, 0));
-//        list[4] = (new Task("Make dinner", true, 5));
-//
 //        Observable<Task> taskObservable = Observable
-//                .fromArray(list)
+//                .fromIterable(DataSource.createTasksList())
+//                .filter(new Predicate<Task>() {
+//                    @Override
+//                    public boolean test(Task task) throws Exception {
+//                        Log.d(TAG, "test: " + Thread.currentThread().getName());
+//                        return task.getDescription().equals("Walk the dog");
+//                    }
+//                })
 //                .subscribeOn(Schedulers.io())
 //                .observeOn(AndroidSchedulers.mainThread());
 //
 //        taskObservable.subscribe(new Observer<Task>() {
 //            @Override
 //            public void onSubscribe(Disposable d) {
-//                Log.d(TAG, "onSubscribe: called");
-//            }
 //
+//            }
 //            @Override
 //            public void onNext(Task task) {
-//                Log.d(TAG, "onNext: : " + task.getDescription());
+//                Log.d(TAG, "onNext: This task matches the description: " + task.getDescription());
 //            }
-//
 //            @Override
 //            public void onError(Throwable e) {
-//                Log.e(TAG, "onError: ", e);
-//            }
 //
+//            }
 //            @Override
 //            public void onComplete() {
-//                Log.d(TAG, "onComplete: done...");
+//
 //            }
 //        });
 
-        List<Task> taskList = new ArrayList<>();
-        taskList.add(new Task("Take out the trash", true, 3));
-        taskList.add(new Task("Walk the dog", false, 2));
-        taskList.add(new Task("Make my bed", true, 1));
-        taskList.add(new Task("Unload the dishwasher", false, 0));
-        taskList.add(new Task("Make dinner", true, 5));
-
         Observable<Task> taskObservable = Observable
-                .fromIterable(taskList)
+                .fromIterable(DataSource.createTasksList())
+                .filter(new Predicate<Task>() {
+                    @Override
+                    public boolean test(Task task) throws Exception {
+                        Log.d(TAG, "test: " + Thread.currentThread().getName());
+                        return task.isComplete();
+                    }
+                })
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread());
 
@@ -79,17 +75,14 @@ public class MainActivity extends AppCompatActivity {
             public void onSubscribe(Disposable d) {
 
             }
-
             @Override
             public void onNext(Task task) {
-                Log.d(TAG, "onNext: : " + task.getDescription());
+                Log.d(TAG, "onNext: This is a completed task: " + task.getDescription());
             }
-
             @Override
             public void onError(Throwable e) {
 
             }
-
             @Override
             public void onComplete() {
 
